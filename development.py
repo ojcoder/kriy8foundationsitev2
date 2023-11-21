ddf94f5312120b5482aacbaf1d8136e3c4271f0e
ojcoder <ojgenius64@gmail.com>
2023-11-21 17:58:02 -0600

    stuff1

--- development.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'mysecretkey'
-MAIL_SERVER = "smtp.gmail.com" # use gmail
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USERNAME = 'project20team@gmail.com'
-MAIL_PASSWORD = 'bloomington@1'
--- production.py
+++
@@ -1,8 +0,0 @@
-DEBUG = False
-SECRET_KEY = 'applewinter'
-MAIL_SERVER = "smtp.gmail.com" # use gmail
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USE_TLS = False
-MAIL_USERNAME = 'project20team'
-MAIL_PASSWORD = 'bloomington@1'
